ca13132032bdc3ecb2c298c2962418cb8370246b
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-04-11 14:17:08 +0200

    Ajout du component header + routeur

--- react-movies/src/App.js
+++ react-movies/src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserList from "./pages/UserList";
 
 export default function App() {
-  return <h1>Hello !!!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coup-de-coeur" element={<UserList />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
